23e4b91a4614b3bbed44ade2c5d224eb6e795047
Eduardo Martínez Vázquez <martinez.vazquez.eduardo@iescamas.es>
2025-06-06 10:50:10 +0200

    Arreglando problemas con la caché

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerPdaCRFN\App_KernelDevDebugContainer([
     'container.build_hash' => 'PdaCRFN',
-    'container.build_id' => '99fd92a0',
-    'container.build_time' => 1749185006,
+    'container.build_id' => 'e548c168',
+    'container.build_time' => 1749197709,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerPdaCRFN');
